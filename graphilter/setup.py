4a0b914d657d6d1ae1511ee056ad67aaba869fa5
Lee Rosenberg <laluk321@gmail.com>
2023-05-21 02:27:08 +0300

    add setup py

---
+++ graphilter/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='graphilter',
+    version=0.1,
+    description='Graph based signal processing tool',
+    author='Lee Rosenberg',
+    packages=[
+        'graphilter',
+    ]
+)
